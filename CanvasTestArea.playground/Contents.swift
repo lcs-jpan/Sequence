a65febafff824ef5867f70ea86173d336882e6b0
Pan Jason <jpan@lcs-mba3130.lcs.on.ca>
2017-10-04 11:55:17 -0400

    Working on the nose

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,18 +21,35 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
-
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
-
+let canvas = Canvas(width: 1000, height: 800)
 // Show where the origin is
 canvas.drawAxes()
+// Make the ears
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 500, centreY: 500, width: 150, height: 150)
+// Make the mouth
+canvas.drawEllipse(centreX: 100, centreY: 100, width: 100, height: 100)
+// Make the ears
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 340, centreY: 640, width: 170, height: 170)
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
-
-
+// Make the eyes
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 400, centreY: 320, width: 130, height: 100)
+// Make the ears
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 700, centreY: 640, width: 170, height: 170)
+// Make the face
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 520, centreY: 400, width: 550, height: 550)
+// Make the eyes
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 400, centreY: 500, width: 130, height: 100, borderWidth: 10)
+// Make the eyes
+canvas.fillColor = Color . black
+canvas.drawEllipse(centreX: 600, centreY: 500, width: 130, height: 100, borderWidth: 10)
+// Make the nose
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 470, centreY: 300, width: 100, height: 80, borderWidth: 10)
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
